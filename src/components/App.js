235838650e0ea855648938ec33c866f16fec85b4
ramankarki <ramankarki40@gmail.com>
2021-03-06 20:15:07 +0545

    renamed HOmepage component to Header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import HomePage from "./HomePage";
+import Header from "./Header";
 import "./App.scss";
 
 class App extends React.Component {
   render() {
     return (
       <div className="container">
-        <HomePage />
+        <Header />
       </div>
     );
   }
