5032d41818e4cd109aefb99d6a1216a73c2ac717
ramankarki <ramankarki40@gmail.com>
2021-03-06 20:14:10 +0545

    kept homepage and header in separate components

--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -1,51 +1,10 @@
 import React, { Component } from "react";
 
-import logo from "../logo.png";
 import "./HomePage.scss";
 
 class index extends Component {
-  state = { isHamburgurActive: false, hamburgurClass: false };
-
-  setHamburgurState = () =>
-    this.setState({
-      isHamburgurActive: !this.state.isHamburgurActive,
-      hamburgurClass: this.state.isHamburgurActive
-        ? "hamburgurBtn"
-        : "hamburgurBtn hamburgurBtn-active",
-    });
-
-  navClass = () => (this.state.isHamburgurActive ? "nav nav-active" : "nav");
-
   render() {
-    return (
-      <header className="header">
-        <div className="header__wrapper">
-          <img className="header__logo" src={logo} alt="logo of my portfolio" />
-          <nav className={`header__nav ${this.navClass()}`}>
-            <a href="#" className="nav__links nav__links nav__links-active">
-              Home
-            </a>
-            <a href="#" className="nav__links">
-              About
-            </a>
-            <a href="#" className="nav__links">
-              Projects
-            </a>
-            <a href="#" className="nav__links">
-              Contact
-            </a>
-          </nav>
-          <div
-            onClick={this.setHamburgurState}
-            className={`header__hamburgurBtn ${this.state.hamburgurClass}`}
-          >
-            <div className="hamburgurBtn__one"></div>
-            <div className="hamburgurBtn__two"></div>
-            <div className="hamburgurBtn__three"></div>
-          </div>
-        </div>
-      </header>
-    );
+    return <div>this is homepage</div>;
   }
 }
 
