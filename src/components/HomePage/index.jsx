15e28f85801b793d4ae2d456b8283e109b8a15be
ramankarki <ramankarki40@gmail.com>
2021-03-08 14:52:03 +0545

    responsive two column homepage

--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -1,10 +1,28 @@
 import React, { Component } from "react";
 
 import "./HomePage.scss";
+import avatar from "../homepage_avatar.jpg";
 
 class index extends Component {
   render() {
-    return <div>this is homepage</div>;
+    return (
+      <section className="homepage">
+        <picture className="homepage__avatar">
+          <img src={avatar} alt="my avatar" />
+        </picture>
+        <div className="homepage__text heroText">
+          <h1 className="heroText__heading">I'm Raman Karki</h1>
+          <h2 className="heroText__subheading">Full stack web developer</h2>
+          <p className="heroText__desc">
+            I build full stack web apps with <span>MERN</span> stack
+          </p>
+          <div className="heroText__callToAction">
+            <button>Hire me</button>
+            <p>or lets build a great project together</p>
+          </div>
+        </div>
+      </section>
+    );
   }
 }
 
