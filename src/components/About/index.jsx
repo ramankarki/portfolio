a2017c7e0da89aa01eb906afd756d54c32b81547
ramankarki <ramankarki40@gmail.com>
2021-03-11 14:35:14 +0545

    responsive about section

---
+++ src/components/About/index.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+import "./About.scss";
+import avatar from "./about_avatar.jpg";
+import illustration from "./about_illustration.svg";
+
+export default class index extends Component {
+  render() {
+    return (
+      <section className="about">
+        <h2 className="about__heading">About me</h2>
+        <div className="about__content">
+          <picture className="about__avatar">
+            <img src={avatar} alt="about avatar" />
+          </picture>
+          <div className="about__desc">
+            <p>
+              I am currently at high school and I love to explore new
+              technologies.
+            </p>
+            <img src={illustration} alt="high school degree icon" />
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
