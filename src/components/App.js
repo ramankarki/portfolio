2b52d63a8613bfc9807d9be7da98ff5cfd5df74b
ramankarki <ramankarki40@gmail.com>
2021-03-11 21:47:08 +0545

    used two column component and used to complete about section content

--- src/components/App.js
+++ src/components/App.js
@@ -4,23 +4,127 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./Header";
 import HomePage from "./HomePage";
 import About from "./About";
-import MyLocation from "./MyLocation";
+import TwoColumn from "./twoColumn";
 import "./App.scss";
 import scrollDownIcon from "./scroll-down.svg";
+import locationIcon from "./location_icon.svg";
+import projectAvatar from "./project_avatar.jpg";
+import fastProjectIcon from "./fast_project_icon.svg";
+import learn1 from "./learn_1.png";
+import htmlIcon from "./html_icon.svg";
+import cssIcon from "./css_icon.svg";
+import jsIcon from "./js_icon.svg";
+import learn2 from "./learn2.png";
+import reactIcon from "./react_icon.svg";
+import reduxIcon from "./redux_icon.svg";
+import learn3 from "./learn3.png";
+import nodeIcon from "./node_icon.svg";
+import mongodbIcon from "./mongodb_icon.svg";
 
 class App extends React.Component {
   render() {
     return (
       <div className="container">
         <Header />
+
         <HomePage />
+
         <div className="scrollDown">
           <span>
             Scroll down <img src={scrollDownIcon} alt="scroll down icon" />
           </span>
         </div>
+
         <About />
-        <MyLocation />
+
+        <TwoColumn>
+          <div className="section__content section__content-reverse">
+            <picture className="section__picture">
+              <iframe
+                title="my location"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14284.14818995936!2d87.28090247132249!3d26.486751988531697!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ef740a4e4e885b%3A0xe1b55cf82fb28f76!2sThulo%20Mill%20Chowk!5e0!3m2!1sen!2snp!4v1615454162698!5m2!1sen!2snp"
+                width="600"
+                height="350"
+                style={{ border: "0" }}
+                allowfullscreen=""
+                loading="lazy"
+              ></iframe>
+            </picture>
+            <div className="section__desc">
+              <img src={locationIcon} alt="location icon" />
+              <p>
+                I live in Nepal, Morang, Biratnagar (56613) and I would love to
+                work with a good community of developers.
+              </p>
+            </div>
+          </div>
+        </TwoColumn>
+
+        <TwoColumn>
+          <div className="section__content">
+            <picture className="section__picture">
+              <img src={projectAvatar} alt="avatar of working on project" />
+            </picture>
+            <div className="section__desc">
+              <img src={fastProjectIcon} alt="location icon" />
+              <p>
+                I enjoy making projects that work and can be useful many people.
+              </p>
+            </div>
+          </div>
+        </TwoColumn>
+
+        <TwoColumn>
+          <div className="section__content section__content-reverse">
+            <picture className="section__picture">
+              <img src={learn1} alt="avatar of working on project" />
+            </picture>
+            <div className="section__desc">
+              <img src={htmlIcon} alt="location icon" />
+              <img src={cssIcon} alt="location icon" />
+              <img src={jsIcon} alt="location icon" />
+              <p>
+                I started by web dev journey from frontend learning basic html,
+                css, javascript and gradually upgraded my skills to then next
+                level.
+              </p>
+            </div>
+          </div>
+        </TwoColumn>
+
+        <TwoColumn>
+          <div className="section__content">
+            <picture className="section__picture">
+              <img src={learn2} alt="avatar of working on project" />
+            </picture>
+            <div className="section__desc">
+              <img src={reactIcon} alt="location icon" />
+              <img src={reduxIcon} alt="location icon" />
+              <p>
+                After some projects, I picked react, redux and worked with APIs
+                to make dynamic client side rendering projects.
+              </p>
+            </div>
+          </div>
+        </TwoColumn>
+
+        <TwoColumn>
+          <div className="section__content section__content-reverse">
+            <picture className="section__picture">
+              <img src={learn3} alt="avatar of working on project" />
+            </picture>
+            <div className="section__desc">
+              <img src={reactIcon} alt="location icon" />
+              <img src={reduxIcon} alt="location icon" />
+              <img src={nodeIcon} alt="location icon" />
+              <img src={mongodbIcon} alt="location icon" />
+              <p>
+                After react, I started learning backend with nodejs, expressjs,
+                mongodb and build my own API to make full stack web apps.
+              </p>
+            </div>
+          </div>
+        </TwoColumn>
       </div>
     );
   }
