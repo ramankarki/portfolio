19503920411c4c9092b564b02acb0fa20f9a9f51
ramankarki <ramankarki40@gmail.com>
2021-03-14 20:40:37 +0545

    added functions to change active nav link on scroll

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,7 +4,11 @@ import logo from "../logo.png";
 import "./Header.scss";
 
 class index extends Component {
-  state = { isHamburgurActive: false, hamburgurClass: false };
+  state = {
+    isHamburgurActive: false,
+    hamburgurClass: false,
+    activeNavLink: "homepage",
+  };
 
   setHamburgurState = () =>
     this.setState({
@@ -16,6 +20,40 @@ class index extends Component {
 
   navClass = () => (this.state.isHamburgurActive ? "nav nav-active" : "nav");
 
+  isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return rect.top <= 32 && rect.bottom > 0;
+  }
+
+  activeLinkClass = (section) =>
+    section === this.state.activeNavLink ? "nav__links-active" : "";
+
+  componentDidMount() {
+    const homepage = document.querySelector("#homepage");
+    const about = document.querySelector("#about");
+    const myTools = document.querySelector("#myTools");
+    const projects = document.querySelector("#projects");
+    const contact = document.querySelector("#contact");
+
+    window.addEventListener("scroll", () => {
+      let activeNavLink = this.state.activeNavLink;
+
+      if (this.isInViewport(contact)) {
+        activeNavLink = "contact";
+      } else if (this.isInViewport(projects)) {
+        activeNavLink = "projects";
+      } else if (this.isInViewport(myTools)) {
+        activeNavLink = "about";
+      } else if (this.isInViewport(about)) {
+        activeNavLink = "about";
+      } else if (this.isInViewport(homepage)) {
+        activeNavLink = "homepage";
+      }
+
+      this.setState({ activeNavLink });
+    });
+  }
+
   render() {
     return (
       <header className="header">
@@ -24,17 +62,26 @@ class index extends Component {
           <nav className={`header__nav ${this.navClass()}`}>
             <a
               href="#homepage"
-              className="nav__links nav__links nav__links-active"
+              className={`nav__links ${this.activeLinkClass("homepage")}`}
             >
               Home
             </a>
-            <a href="#about" className="nav__links">
+            <a
+              href="#about"
+              className={`nav__links ${this.activeLinkClass("about")}`}
+            >
               About
             </a>
-            <a href="#projects" className="nav__links">
+            <a
+              href="#projects"
+              className={`nav__links ${this.activeLinkClass("projects")}`}
+            >
               Projects
             </a>
-            <a href="#contact" className="nav__links">
+            <a
+              href="#contact"
+              className={`nav__links ${this.activeLinkClass("contact")}`}
+            >
               Contact
             </a>
           </nav>
