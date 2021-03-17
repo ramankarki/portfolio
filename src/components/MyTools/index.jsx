e1b03ae17cf61c2d99ac7c82cbb277cfacaa2cbd
ramankarki <ramankarki40@gmail.com>
2021-03-17 11:42:27 +0545

    added tools icon in desc and updated tools

--- src/components/MyTools/index.jsx
+++ src/components/MyTools/index.jsx
@@ -4,6 +4,7 @@ import "./MyTools.scss";
 import webDesign from "./webdesign.svg";
 import frontend from "./frontend.svg";
 import backend from "./backend.svg";
+import toolsIcon from "./tools_icon.svg";
 
 class index extends Component {
   render() {
@@ -11,7 +12,10 @@ class index extends Component {
       <section className="myTools" id="myTools">
         <div className="myTools__heading">
           <h2>My Tools</h2>
-          <p>I use frequently for my projects</p>
+          <p>
+            I use frequently for my projects{" "}
+            <img src={toolsIcon} alt="tools icon" />
+          </p>
         </div>
         <div className="myTools__table toolsTable">
           <section className="toolsTable__webDesign">
@@ -27,8 +31,7 @@ class index extends Component {
           <section className="toolsTable__frontend">
             <img src={frontend} alt="icon for frontend coding" />
             <h3>Frontend</h3>
-            <p>HTML</p>
-            <p>CSS</p>
+            <p>HTML & CSS</p>
             <p>JavaScript</p>
             <p>Bootstrap</p>
             <p>Sass</p>
@@ -39,16 +42,14 @@ class index extends Component {
           <section className="toolsTable__backend">
             <img src={backend} alt="icon for backend coding" />
             <h3>Backend</h3>
-            <p>Node</p>
-            <p>Express</p>
-            <p>MongoDB</p>
-            <p>Mongoose</p>
+            <p>NodeJS & ExpressJS</p>
+            <p>MongoDB & MongooseJS</p>
             <p>Postman</p>
             <p>npm scripts</p>
             <p>Terminal</p>
           </section>
           <section className="toolsTable__someOthers">
-            <h3>Some others</h3>
+            <h3>Some other tech I love</h3>
             <p>VScode, Codepen, Git, Github, Bitbucket, Python, Ubuntu</p>
           </section>
         </div>
