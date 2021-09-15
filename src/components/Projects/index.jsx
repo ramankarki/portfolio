ea87412809de02ade3a0e969f9439fed45599a81
ramankarki <ramankarki40@gmail.com>
2021-09-15 08:12:54 +0545

    add easy stationary project

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,22 +1,23 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import "./Projects.scss";
-import liveIcon from "./img/internet_icon.svg";
-import githubIcon from "./img/github_icon.svg";
-import chatAppSS from "./img/chat_app.png";
-import folderListAppSS from "./img/folder_list_app.png";
-import weatherAppSS from "./img/weather_app.png";
-import jobSearchSS from "./img/job_search_app.png";
-import reactIcon from "./img/react.svg";
-import reduxIcon from "./img/redux.svg";
-import bootstrapIcon from "./img/bootstrap.svg";
-import sassIcon from "./img/sass.svg";
-import nodeIcon from "./img/node.svg";
-import mongodbIcon from "./img/mongodb.svg";
-import htmlIcon from "./img/html.svg";
-import cssIcon from "./img/css.svg";
-import jsIcon from "./img/js.svg";
-import projectIcon from "./img/project_icon.svg";
+import './Projects.scss';
+import liveIcon from './img/internet_icon.svg';
+import githubIcon from './img/github_icon.svg';
+import chatAppSS from './img/chat_app.png';
+import folderListAppSS from './img/folder_list_app.png';
+import weatherAppSS from './img/weather_app.png';
+import jobSearchSS from './img/job_search_app.png';
+import easyStationarySS from './img/easy-stationary.png';
+import reactIcon from './img/react.svg';
+import reduxIcon from './img/redux.svg';
+import bootstrapIcon from './img/bootstrap.svg';
+import sassIcon from './img/sass.svg';
+import nodeIcon from './img/node.svg';
+import mongodbIcon from './img/mongodb.svg';
+import htmlIcon from './img/html.svg';
+import cssIcon from './img/css.svg';
+import jsIcon from './img/js.svg';
+import projectIcon from './img/project_icon.svg';
 
 export default class index extends Component {
   render() {
@@ -25,12 +26,63 @@ export default class index extends Component {
         <div className="projects__heading">
           <h2>My Projects</h2>
           <p>
-            Some of my projects I build while learning{" "}
+            Some of my projects I build while learning{' '}
             <img src={projectIcon} alt="project icon" />
           </p>
         </div>
 
         <div className="projects__cardsContainer">
+          <article className="card">
+            <img
+              className="card__projectSS"
+              src={easyStationarySS}
+              alt="easy stationary screen shot"
+            />
+            <div className="card__content">
+              <h3 className="card__heading">E-commerce site (working on it)</h3>
+              <p className="card__subheading">Featured project</p>
+              <p className="card__desc">
+                An e-commerce site with client side rendering, build with React
+                and Redux. It has API on backend build with NodeJS, ExpressJS,
+                MongoDB, MongooseJS with JWT authentication and authorization.
+              </p>
+              <img
+                className="card__techIcon"
+                src={reactIcon}
+                alt="react icon"
+              />
+              <img
+                className="card__techIcon"
+                src={reduxIcon}
+                alt="redux icon"
+              />
+              <img className="card__techIcon" src={sassIcon} alt="sass icon" />
+              <img className="card__techIcon" src={nodeIcon} alt="node icon" />
+              <img
+                className="card__techIcon"
+                src={mongodbIcon}
+                alt="mongodb icon"
+              />
+              <span>real time</span>
+              <div className="card__button">
+                <a
+                  href="https://easy-stationary.netlify.app"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={liveIcon} alt="live on internet icon" /> See live
+                </a>
+                <a
+                  href="https://github.com/ramankarki/easy-stationary"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={githubIcon} alt="github logo" />
+                </a>
+              </div>
+            </div>
+          </article>
+
           <article className="card">
             <img
               className="card__projectSS"
@@ -223,14 +275,14 @@ export default class index extends Component {
         </div>
 
         <p className="projects__moreProject">
-          You can visit my{" "}
+          You can visit my{' '}
           <a
             href="https://github.com/ramankarki"
             target="_blank"
             rel="noreferrer"
           >
             Github Profile
-          </a>{" "}
+          </a>{' '}
           to see more projects I have made until now.
         </p>
       </section>
