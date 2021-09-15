a60c54e30d05a5f03896d10644e515a2c406dda7
ramankarki <ramankarki40@gmail.com>
2021-09-15 07:23:32 +0545

    update tools

--- src/components/MyTools/index.jsx
+++ src/components/MyTools/index.jsx
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import "./MyTools.scss";
-import webDesign from "./webdesign.svg";
-import frontend from "./frontend.svg";
-import backend from "./backend.svg";
-import toolsIcon from "./tools_icon.svg";
+import './MyTools.scss';
+import webDesign from './webdesign.svg';
+import frontend from './frontend.svg';
+import backend from './backend.svg';
+import toolsIcon from './tools_icon.svg';
 
 class index extends Component {
   render() {
@@ -13,7 +13,7 @@ class index extends Component {
         <div className="myTools__heading">
           <h2>My Tools</h2>
           <p>
-            I use frequently for my projects{" "}
+            I use frequently for my projects{' '}
             <img src={toolsIcon} alt="tools icon" />
           </p>
         </div>
@@ -22,11 +22,9 @@ class index extends Component {
             <img src={webDesign} alt="icon for responsive layout web design" />
             <h3>Web Design</h3>
             <p>Figma</p>
-            <p>AdobeXD</p>
             <p>Inkscape</p>
             <p>Diagrams</p>
             <p>Pen & Paper</p>
-            <p>Dribbble</p>
           </section>
           <section className="toolsTable__frontend">
             <img src={frontend} alt="icon for frontend coding" />
@@ -37,20 +35,19 @@ class index extends Component {
             <p>Sass</p>
             <p>React</p>
             <p>Redux</p>
-            <p>React & Redux Dev extensions</p>
           </section>
           <section className="toolsTable__backend">
             <img src={backend} alt="icon for backend coding" />
             <h3>Backend</h3>
-            <p>NodeJS & ExpressJS</p>
-            <p>MongoDB & MongooseJS</p>
-            <p>Postman</p>
-            <p>npm scripts</p>
-            <p>Terminal</p>
+            <p>NodeJS</p>
+            <p>ExpressJS</p>
+            <p>MongoDB</p>
+            <p>MongooseJS</p>
+            <p>Postman (for API testing)</p>
           </section>
           <section className="toolsTable__someOthers">
             <h3>Some other tech I love</h3>
-            <p>VScode, Codepen, Git, Github, Bitbucket, Python, Ubuntu</p>
+            <p>VScode, Codepen, Git, Github, Bitbucket, Python</p>
           </section>
         </div>
       </section>
